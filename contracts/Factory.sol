23130ba291d7f4f96f34299e89ed415aff557644
Willem Veelenturf <willem.veelenturf@gmail.com>
2016-05-22 23:49:14 +0200

    add create for paswordchallange

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -4,11 +4,21 @@ contract Factory {
 
     address[] parties;
 
-    function createParty()  returns (address addr){
+    function getParties() constant returns (address[]){
+        return parties;
+    }
+
+    function createParty() returns (address addr){
         var party = new Party();
         parties.push(party);
         created(party);
         return party;
+    }
+
+    function createPasswordChallenge(bytes20 response, bytes32 salt) returns (address addr){
+        var challenge = new PasswordChallenge(response, salt);
+        created(challenge);
+        return challenge;
     }
 
 }
